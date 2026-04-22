705a9884617b3837cf9c5c9511a4c95c849c42b6
Jonathan4542 <jonathan.ferreira62@aluno.ifce.edu.br>
2026-04-22 10:27:50 -0300

    Create conta_corrente.py

---
+++ contas/conta_corrente.py
@@ -0,0 +1,16 @@
+from contas.conta import Conta
+
+class ContaCorrente(Conta):
+    def __init__(self, numero, cliente, limite=500.0):
+        super().__init__(numero, cliente)
+        self.limite = limite
+
+    # Polimorfismo: Regra de saque específica para Conta Corrente [cite: 47, 48]
+    def sacar(self, valor):
+        if valor > 0 and (self._saldo + self.limite) >= valor:
+            self._saldo -= valor
+            self.historico.adicionar_operacao(f"Saque Conta Corrente: R$ {valor:.2f}")
+            print(f"Saque de R$ {valor:.2f} realizado.")
+            return True
+        print("Saque negado: Saldo e limite insuficientes.") [cite: 57, 59]
+        return False
