ac01021191f312087626270480dc9e9fc8c4e8e8
Jonathan4542 <jonathan.ferreira62@aluno.ifce.edu.br>
2026-04-22 10:34:06 -0300

    historico.py

--- operacoes/historico.py
+++ operacoes/historico.py
@@ -1,9 +1,13 @@
+from operacoes.operacao import Operacao
+
 class Historico:
     def __init__(self):
-        self._operacoes = [] # Encapsulamento [cite: 53, 54]
+        self._operacoes = []
 
-    def adicionar_operacao(self, operacao):
-        self._operacoes.append(operacao)
+    def adicionar_operacao(self, detalhes):
+        # Instanciando o objeto Operacao conforme exigido
+        nova_operacao = Operacao(detalhes)
+        self._operacoes.append(nova_operacao)
 
     def listar(self):
         if not self._operacoes:
