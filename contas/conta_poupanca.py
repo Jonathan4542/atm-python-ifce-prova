a51be3991788e607ebbd5a32ac428639a34acbe4
Jonathan4542 <jonathan.ferreira62@aluno.ifce.edu.br>
2026-04-22 10:28:31 -0300

    Create conta_poupanca.py

---
+++ contas/conta_poupanca.py
@@ -0,0 +1,12 @@
+from contas.conta import Conta
+
+class ContaPoupanca(Conta):
+    # Polimorfismo: Regra de saque específica para Poupança [cite: 47, 48]
+    def sacar(self, valor):
+        if valor > 0 and self._saldo >= valor:
+            self._saldo -= valor
+            self.historico.adicionar_operacao(f"Saque Poupança: R$ {valor:.2f}")
+            print(f"Saque de R$ {valor:.2f} realizado com sucesso.")
+            return True
+        print("Saque negado: Saldo insuficiente na poupança.") [cite: 57, 59]
+        return False
